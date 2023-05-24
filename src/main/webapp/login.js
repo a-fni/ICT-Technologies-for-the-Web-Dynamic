dc17ef73aaa4da86890ec05116322f64bdd447ee
a-fni <andreaferrarini.dev@gmail.com>
2023-05-24 10:00:58 +0200

    Remov login form action

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,13 +1,14 @@
 window.addEventListener("load", () => {
   const loginForm = document.querySelector("#login");
   loginForm.addEventListener("submit", async event => {
+    event.preventDefault();
     console.log("Login form submitted");
     const res = await fetch("login", {
       method: "POST",
       body: new FormData(loginForm),
     });
     const data = await res.json();
-    console.log("Parsed reposnse", data);
+    console.log("Parsed response", data);
 
     if (data.success) {
       sessionStorage.setItem("username", data.username);
