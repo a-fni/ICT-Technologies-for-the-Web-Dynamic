97ffd5b7ac5ca30505b3891d1898a4ab41a68bbd
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-05-24 09:42:57 +0200

    login error message

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,17 +1,20 @@
 window.addEventListener("load", () => {
   const loginForm = document.querySelector("#login");
   loginForm.addEventListener("submit", async event => {
+    console.log("Login form submitted");
     const res = await fetch("login", {
       method: "POST",
       body: new FormData(loginForm),
     });
     const data = await res.json();
+    console.log("Parsed reposnse", data);
 
     if (data.success) {
       window.location.href = "index.html";
       sessionStorage.setItem("username", data.username);
     } else {
-      alert("Login failed");
+      const error = document.querySelector("#error-message");
+      error.textContent = data.message;
     }
   });
 });
